15266a2de79a0ebf44eccc54d6dc2bb15965fd81
mhavril39 <mhavril39@hotmail.com>
2015-02-23 11:02:22 -0500

    initial commit

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ class MyWebApp < Sinatra::Base
   get '/' do
     @holidays_now = HolidApi.get(country: 'US', month: (Date.today.strftime("%m")))
     @holidays_birth = HolidApi.get(country: 'US', year: '1993', month: '1')
+    @holidays_yourbirth = HolidApi.get(year: params['year'], month: params['month'])
+    puts params
     erb :index
   end
 end
